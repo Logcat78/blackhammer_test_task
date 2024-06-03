d41ecf2d02b1c6f4bbfed8d8e2f8e6c6b58a6442
Станислав <MarioBlamp@yandex.ru>
2024-06-03 19:09:10 +0300

    add work gesture

--- app/src/main/java/com/task/blackhammer_test_task/GestureAccessibilityService.kt
+++ app/src/main/java/com/task/blackhammer_test_task/GestureAccessibilityService.kt
@@ -1,34 +1,54 @@
 package com.task.blackhammer_test_task
 
+import android.accessibilityservice.AccessibilityGestureEvent
 import android.accessibilityservice.AccessibilityService
 import android.accessibilityservice.GestureDescription
 import android.graphics.Path
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import android.view.MotionEvent
 import android.view.accessibility.AccessibilityEvent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class GestureAccessibilityService: AccessibilityService(){
     override fun onAccessibilityEvent(p0: AccessibilityEvent?) {
         Log.d("gp", "Event")
     }
 
+    override fun onServiceConnected() {
+        super.onServiceConnected()
+        Log.d("gp", "Event2323233")
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(1000)
+            while (true){
+                swipe()
+            }
+        }
+
+    }
+
+    override fun onMotionEvent(event: MotionEvent) {
+        super.onMotionEvent(event)
+        Log.d("gp",  "MotionEvent")
+    }
     fun swipe(){
         val path: Path = Path()
-        path.moveTo(100f, 10f)
-        path.lineTo(100f, 100f)
+        path.moveTo(500f, 500f)
+        path.lineTo(100f, 500f)
 
         val gesture = GestureDescription.Builder()
             .addStroke(GestureDescription.StrokeDescription(
                 path,
-                100L,
-                100L,
+                0,
+                500,
             ))
 
 
-        Log.d("gp", "Event123")
-
-        dispatchGesture(gesture.build(), object : GestureResultCallback() {
+       dispatchGesture(gesture.build(), object : GestureResultCallback() {
             override fun onCompleted(gestureDescription: GestureDescription?) {
                 Log.d("gp","onCompleted")
             }
@@ -37,6 +57,7 @@ class GestureAccessibilityService: AccessibilityService(){
                 Log.d("gp","onCancelled")
             }
         }, null)
+
     }
 
     override fun onInterrupt() {
